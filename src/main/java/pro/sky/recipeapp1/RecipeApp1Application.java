ca65065493d1b4db56f1581c667237de524f8630
karen gevorgyan <karen-gev.22@mail.ru>
2023-01-13 12:49:25 +0300

    first

---
+++ src/main/java/pro/sky/recipeapp1/RecipeApp1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.recipeapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApp1Application.class, args);
+    }
+
+}
