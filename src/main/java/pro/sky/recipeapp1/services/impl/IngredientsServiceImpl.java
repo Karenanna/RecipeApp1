db22429aec14d5ab219ac0f10550c38d213d4608
karen gevorgyan <karen-gev.22@mail.ru>
2023-02-16 23:27:19 +0300

    Сделал работу над ошибками

--- src/main/java/pro/sky/recipeapp1/services/impl/IngredientsServiceImpl.java
+++ src/main/java/pro/sky/recipeapp1/services/impl/IngredientsServiceImpl.java
@@ -3,13 +3,23 @@ package pro.sky.recipeapp1.services.impl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import pro.sky.recipeapp1.model.Ingredients;
 import pro.sky.recipeapp1.services.FilesService;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.TreeMap;
 
@@ -84,9 +94,22 @@ public class IngredientsServiceImpl implements IngredientsService {
     }
     @Override
     public void importFile(MultipartFile file) {
+        @PostMapping(value = "/import/{dataNameFile}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+        public ResponseEntity<Void> upLoadeFiles(@PathVariable String dataFileName, @RequestParam MultipartFile file){
+            filesService.cleanFile(dataFileName);
+            File dataFile = filesService.getDataFile(dataFileName);
+            try (FileOutputStream fos = new FileOutputStream(dataFile)){
+                IOUtils.copy(file.getInputStream(), fos);
+                return ResponseEntity.ok().build();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
 
     }
 }
+
 
 
 
--- src/main/java/pro/sky/recipeapp1/services/impl/RecipeServiceImpl.java
+++ src/main/java/pro/sky/recipeapp1/services/impl/RecipeServiceImpl.java
@@ -3,8 +3,15 @@ package pro.sky.recipeapp1.services.impl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import pro.sky.recipeapp1.model.Recipe;
 import pro.sky.recipeapp1.services.FilesService;
@@ -13,6 +20,8 @@ import pro.sky.recipeapp1.services.RecipeService;
 
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
@@ -101,6 +110,18 @@ public class RecipeServiceImpl implements RecipeService {
     }
     @Override
     public void importFile(MultipartFile file) {
+        @PostMapping(value = "/import/{dataNameFile}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+                public ResponseEntity<Void> upLoadeFiles(@PathVariable String dataFileName, @RequestParam MultipartFile file){
+            filesService.cleanFile(dataFileName);
+            File dataFile = filesService.getDataFile(dataFileName);
+            try (FileOutputStream fos = new FileOutputStream(dataFile)){
+                IOUtils.copy(file.getInputStream(), fos);
+                return ResponseEntity.ok().build();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
 
     }
 }
